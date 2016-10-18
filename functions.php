74851304f0180daec2a2ba1e86ac557fc8038649
Ivan26ru <it-1ivan@ya.ru>
2016-10-18 20:07:11 +0300

    сброс css, убрал bootstrap'

--- functions.php
+++ functions.php
@@ -98,7 +98,6 @@ if (!function_exists('add_scripts')) { // если ф-я уже есть в до
 	    if(is_admin()) return false; // если мы в админке - ничего не делаем
 	    wp_deregister_script('jquery'); // выключаем стандартный jquery
 	    wp_enqueue_script('jquery','//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js','','',true); // добавляем свой
-	    wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.min.js','','',true); // бутстрап
 	    wp_enqueue_script('main', get_template_directory_uri().'/js/main.js','','',true); // и скрипты шаблона
 	}
 }
@@ -107,7 +106,7 @@ add_action('wp_print_styles', 'add_styles'); // приклеем ф-ю на до
 if (!function_exists('add_styles')) { // если ф-я уже есть в дочерней теме - нам не надо её определять
 	function add_styles() { // добавление стилей
 	    if(is_admin()) return false; // если мы в админке - ничего не делаем
-	    wp_enqueue_style( 'bs', get_template_directory_uri().'/css/bootstrap.min.css' ); // бутстрап
+	    wp_enqueue_style( 'bs', get_template_directory_uri().'/css/normalize.css' ); // сброс css
 		// wp_enqueue_style( 'main', get_template_directory_uri().'/style.css' ); // основные стили шаблона
 		$date_css=date('YmdHis');
 		wp_enqueue_style( 'main', get_template_directory_uri().'/style.css?' . $date_css); // мои стили шаблона ВСЕГДА ОБНОВЛЯЮТСЯ
