1ab3b7397f069f50eabea1b164ac6d19371c6d9c
Ivan26ru <it-1ivan@ya.ru>
2016-10-19 20:19:45 +0300

    Сброс css и два меню

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 	<link rel="alternate" type="application/rss+xml" title="RSS" href="<?php bloginfo('rss_url'); ?>">
 	<link rel="alternate" type="application/rss+xml" title="Comments RSS" href="<?php bloginfo('comments_rss2_url'); ?>">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/style.css">
 	 <!--[if lt IE 9]>
 	 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -23,6 +24,15 @@
 </head>
 <body <?php body_class(); // все классы для body ?>>
 	<header>
+			<?php $args = array( // опции для вывода верхнего меню, чтобы они работали, меню должно быть создано в админке
+			'theme_location' => 'topup', // идентификатор меню, определен в register_nav_menus() в function.php
+			'container'=> 'nav', // обертка списка
+			'menu_class' => 'bottom-menu', // класс для ul
+	  		'menu_id' => 'bottom-nav', // id для ul
+  			);
+			wp_nav_menu($args); // выводим верхнее меню
+		?>
+
 		<?php $args = array( // опции для вывода верхнего меню, чтобы они работали, меню должно быть создано в админке
 			'theme_location' => 'top', // идентификатор меню, определен в register_nav_menus() в function.php
 			'container'=> 'nav', // обертка списка
