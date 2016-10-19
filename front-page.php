dcd62d463189db1222f41b0a2f4ed867752f5cdf
Ivan26ru <it-1ivan@ya.ru>
2016-10-19 20:15:54 +0300

    создано меню и главная страница

---
+++ front-page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Шаблон главной страницы (front-page.php)
+ * @package WordPress
+ * @subpackage your-clean-template
+ */
+get_header(); // подключаем header.php ?>
+
+<?php get_footer(); // подключаем footer.php ?>
