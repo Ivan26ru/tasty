d21b38227910fa081d43fd5a4db28194cc7522a5
Ivan26ru <it-1ivan@ya.ru>
2016-10-20 22:33:28 +0300

    краское описание новости

--- front-page.php
+++ front-page.php
@@ -13,6 +13,12 @@ get_header(); // подключаем header.php ?>
 			<div class="block block-left">
 				<a href="#" class="zagolovok"><span class="line">МИКСОЛОГИЯ</span></a>
 				<div class="mixologiya"></div>
+				<div class="news">
+					<span class="data">09.08.2016</span>
+					<p class="title">Обзор жидкости ZE-PAR - Креатив и фантазия и все такое</p>
+					<p class="text">Баланс представления, качества и цены - залог успеха. Сегодня я хочу рассказать о линейке жидкости ZE-PAR, оригинальной в подаче, вкусной в содержании, доступной в цене</p>
+					<a href="#" class="read-all">читать полностью...</a>
+				</div>
 
 			</div>
 			<div class="block block-right">
