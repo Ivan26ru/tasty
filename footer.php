ea99db296295fd17cd645c2a4ffaeda18f74b541
Ivan26ru <it-1ivan@ya.ru>
2016-10-21 12:35:02 +0300

    полоски в футере на весь экран

--- footer.php
+++ footer.php
@@ -7,23 +7,26 @@
 ?>
 	<footer>
 	<!-- по центру -->
-		<div class="w1200">
-			<!-- футер 1(верхний) -->
 			<div class="footer footer-1">
-							<?php $args = array( // опции для вывода нижнего меню,
-				'theme_location' => 'bottom', // идентификатор меню, определен в register_nav_menus() в function.php
-				'container'=> false, // обертка списка, false - это ничего
-				'menu_class' => 'bottom-menu', // класс для ul
-		  		'menu_id' => 'bottom-nav', // id для ul
-		  	);
-			wp_nav_menu($args); // выводим нижние меню
-			?>
+				<div class="w1200">
+				<!-- футер 1(верхний) -->
+					<?php $args = array( // опции для вывода нижнего меню,
+					'theme_location' => 'bottom', // идентификатор меню, определен в register_nav_menus() в function.php
+					'container'=> false, // обертка списка, false - это ничего
+					'menu_class' => 'bottom-menu', // класс для ul
+			  		'menu_id' => 'bottom-nav', // id для ul
+			  		);
+					wp_nav_menu($args); // выводим нижние меню
+					?>
+				</div>
 			</div>
 			<!-- футер 2(нижний) -->
 			<div class="footer footer-2">
-				футер 2
+				<div class="w1200">
+					футер 2
+				</div>
 			</div>
-		</div>
+
 	</footer>
 <?php wp_footer(); // необходимо для работы плагинов и функционала  ?>
 </body>
