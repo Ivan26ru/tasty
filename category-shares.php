aa9ef045ea9394e3597e46017e6e413fe35086b5
Ivan26ru <it-1ivan@ya.ru>
2016-10-23 23:37:21 +0300

    страница акции

---
+++ category-shares.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Шаблон лучшие девайсы (category-best_device.php)
+ * @package WordPress
+ * @subpackage your-clean-template
+ */
+get_header(); // подключаем header.php ?>
+
+
+
+<?php get_sidebar(); // подключаем footer.php ?>
+<?php get_footer(); // подключаем footer.php ?>
