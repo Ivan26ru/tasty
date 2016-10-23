12aacdf98700afa172831cf4976336216d1f77da
Ivan26ru <it-1ivan@ya.ru>
2016-10-24 01:17:58 +0300

    конткакты акции церез таблицу

--- category-shares.php
+++ category-shares.php
@@ -6,7 +6,39 @@
  */
 get_header(); // подключаем header.php ?>
 
-
-
+<!-- начало поста -->
+<h1 class="akciya-h1">Акция от GearBest — Good Deals in Clearance Special</h1>
+<div class="akciya-container clearfix">
+	<div class="container-akciya-kontakt">
+		<!-- блок с основной картинкой -->
+		<div class="akciya-div-logo">
+			<img src="http://placehold.it/540x304/2ecc71/ecf0f1">
+		</div>
+		<div class="akciya-kontakt">
+			<table>
+				<tr>
+					<td>Компания:</td>
+					<td>геар бест</td>
+				</tr>
+				<tr>
+					<td>Рейтинг:</td>
+					<td>+12 зеленый</td>
+				</tr>
+				<tr>
+					<td>Отзывы:</td>
+					<td>2 зеленый</td>
+				</tr>
+				<tr>
+					<td>Телефон:</td>
+					<td>8(955)323-12-12</td>
+				</tr>
+				<tr>
+					<td>Адрес:</td>
+					<td>Россия, г.Москва, ул.Смольная, дом 24а</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
 <?php get_sidebar(); // подключаем footer.php ?>
 <?php get_footer(); // подключаем footer.php ?>
