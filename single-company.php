d67bc7ff0525063630fe564ff220e22f4b950b90
Ivan26ru <it-1ivan@ya.ru>
2016-10-26 23:21:55 +0300

    готова верстка страницы компания

--- single-company.php
+++ single-company.php
@@ -14,7 +14,7 @@ get_header(); // подключаем header.php ?>
 	<div class="container-akciya-kontakt clearfix">
 		<!-- блок с основной картинкой -->
 		<div class="company-div-logo">
-<!-- 		<?php if ( has_post_thumbnail() ) the_post_thumbnail('504x304','class=post-shares-img'); // выводим миниатюру поста, если есть 
+<!-- 		<?php if ( has_post_thumbnail() ) the_post_thumbnail('504x304','class=post-shares-img'); // выводим миниатюру поста, если есть
 else echo '<img class="post-shares-img" src="http://placehold.it/540x304/2ecc71/ecf0f1">';?> -->
 <img class="post-shares-img" src="http://placehold.it/90x72/2ecc71/ecf0f1">
 		</div>
@@ -23,33 +23,41 @@ else echo '<img class="post-shares-img" src="http://placehold.it/540x304/2ecc71/
 			<table class="company-kontakt-table">
 <h1 class="company-h1"><span class="line"><?php the_title(); ?></span></h1>
 				<tr>
-					<td>Рейтинг:</td>
-					<td>
+					<td class="brd w100">Рейтинг:</td>
+					<td class="brd w180">
 						<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
 						<span class="green font-rr">+12</span>
 						<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
 					</td>
-					<td class="null"></td>
-					<td class="null"></td>
-										<td>Телефон:</td>
-					<td><?php echo get_post_custom_values(shares_telefon)[0]; //вывод произвольного поля?></td>
+					<td class="null">&nbsp;</td>
+					<td class="null">&nbsp;</td>
+					<td class="brd w70">Телефон:</td>
+					<td class="brd w330"><?php echo get_post_custom_values(shares_telefon)[0]; //вывод произвольного поля?></td>
 
 				</tr>
 				<tr>
-					<td>Отзывы:</td>
-					<td><span class="green font-rr"><?php comments_number('пока нет', '1', '%'); ?></span></td>
-					<td class="null"></td>
-					<td class="null"></td>
-										<td>Адрес:</td>
-					<td><?php echo get_post_custom_values(shares_adres)[0]; //вывод произвольного поля?></td>
-					<td class="text-center" colspan="2"><a href="#" class="btn-green">Перейти на сайт</a></td>
+					<td class="brd">Отзывы:</td>
+					<td class="brd"><span class="green font-rr"><?php comments_number('пока нет', '1', '%'); ?></span></td>
+					<td class="null">&nbsp;</td>
+					<td class="null">&nbsp;</td>
+					<td class="brd ">Адрес:</td>
+					<td class="brd"><?php echo get_post_custom_values(shares_adres)[0]; //вывод произвольного поля?></td>
+				</tr>
+				<tr>
+					<td class="null">&nbsp;</td>
+					<td class="null">&nbsp;</td>
+					<td class="null">&nbsp;</td>
+					<td class="null">&nbsp;</td>
+					<td class="text-right" colspan="2">
+						<a href="#" class="btn-green">Перейти на сайт</a>
+					</td>
 				</tr>
 			</table>
 		</div>
 	</div>
 	<!-- коонец блок с картинкой и контактами -->
 	<div class="o-akcii mt33">
-		<p class="podzagolovok"><span class="line">О акции</span></p>
+		<p class="podzagolovok"><span class="line">О компании</span></p>
 		<div class="text-other">
 			<?php add_filter('the_content','htm_image_content_filter',11); ?>
 <?php the_content(); // контент без картинок?>
@@ -65,14 +73,14 @@ else echo '<img class="post-shares-img" src="http://placehold.it/540x304/2ecc71/
 						<img class="post-img" src="http://placehold.it/112x512/2ecc71/ecf0f1">
 						<img class="post-img" src="http://placehold.it/51x52/2ecc71/ecf0f1">
 						<img class="post-img" src="http://placehold.it/112x112/2ecc71/ecf0f1">
-<!--  
+<!--
 <?php $str= get_the_content();
 preg_match_all('/src="([^"]+)"/i', $str, $matches);
 $img_urls = $matches[1]; ?>
 <?php if($img_urls) { ?>
 <? foreach ($img_urls as $img_url) {?>
 <img class="post-img" src="<?php echo $img_url; ?>" />
-<?php }} 
+<?php }}
 ?>
 
 -->
