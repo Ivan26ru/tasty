588e326f1b0a606ec993ba234a463cd57d83dbec
Ivan26ru <it-1ivan@ya.ru>
2016-10-27 18:39:01 +0300

    верстка таблицей списка компаний

--- category-company.php
+++ category-company.php
@@ -7,48 +7,37 @@
 get_header(); // подключаем header.php ?>
 
 <!-- Начало рубрики-->
-<div class="header-category clearfix">
-	<p class="podzagolovok fs12 nazvanie"><span class="line">название</span></p>
-	<p class="podzagolovok fs12 company"><span class="line">компания</span></p>
-</div>
+<table class="list-company">
+<tr>
+	<th class="th-null">&nbsp;</th>
+	<th class="th-name"><p class="podzagolovok"><span class="line">Название</span></p></th>
+	<th class="th-otziv"><p class="podzagolovok"><span class="line">Отзывы</span></p></th>
+	<th class="th-rejting"><p class="podzagolovok"><span class="line">Рейтинг</span></p></th>
+</tr>
+
+<tr class="company-content">
+	<td class="td-img">
+		<div class="company-div-img"><img src="http://placehold.it/150x150/2ecc71/ecf0f1" alt=""></div>
+	</td>
+	<td class="td-name">
+		<a href="<?php the_permalink() ?>"><p class="podzagolovok fs16"><?php the_title(); ?></p></a>
+		<p class="text-other fs12">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt illum architecto commodi recusandae doloremque. Expedita quis non quam consectetur molestiae provident iste ducimus, quaerat eum repellendus ut excepturi iusto! Veniam enim commodi laboriosam totam, facilis ipsam quaerat obcaecati omnis numquam! Ratione voluptatibus maxime ipsa sapiente temporibus voluptate fuga, modi minus.</p>
+	</td>
+	<td class="td-otziv">
+		<span class="green font-rr"><?php comments_number('пока нет', '1', '%'); ?></span>
+	</td>
+	<td class="td-rejting">
+		<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+		<span class="green font-rr">+12</span>
+		<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+	</td>
+</tr>
+
+
+</table>
+
 <!-- все посты -->
 <div class="post-shares-all">
-
-	<?php if (have_posts()) : while (have_posts()) : the_post(); // если посты есть - запускаем цикл wp ?>
-
-
-<!-- начало поста -->
-<div class="post-shares clearfix">
-	<!-- миниатюра -->
-	<div class="post-shares-div-img">
-<?php if ( has_post_thumbnail() ) the_post_thumbnail(thumbnail,'class=post-shares-img'); // выводим миниатюру поста, если есть
-else echo '<img class="post-shares-img" src="http://placehold.it/220x120/2ecc71/ecf0f1">';?>
-
-	</div>
-	<!-- содежрание поста -->
-	<div class="post-shares-content">
-		<a href="<?php the_permalink() ?>"><p class="podzagolovok fs16"><?php the_title(); ?></p></a>
-		<p class="text-other fs12"><?php the_truncated_post( 400 ); ?></p>
-	</div>
-	<!-- компания -->
-	<div class="post-shares-name-company">
-		<p class="shares-name-company"><?php echo get_post_custom_values(shares_company)[0]; //вывод произвольного поля?></p>
-	</div>
-		<!-- компания -->
-	<div class="post-shares-name-company">
-		<p class="shares-name-company"><?php echo get_post_custom_values(shares_company)[0]; //вывод произвольного поля?></p>
-	</div>
-	<!-- дата поста -->
-<!-- 	<span class="post-shares-data">
-		<?php the_time('F j, Y'); ?>
-	</span> -->
-</div>
-<!-- конец поста -->
-
-
-	<?php endwhile; // конец цикла
-	else: echo '<h2>Нет записей.</h2>'; endif; // если записей нет, напишим "простите" ?>
-	<?php pagination(); // пагинация, функция нах-ся в function.php ?>
 
 
 
@@ -59,4 +48,3 @@ else echo '<img class="post-shares-img" src="http://placehold.it/220x120/2ecc71/
 
 <?php get_sidebar(); // подключаем footer.php ?>
 <?php get_footer(); // подключаем footer.php ?>
-
