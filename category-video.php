2249a6647a2efd0fbe319eae35cb54338c3b9041
Ivan26ru <it-1ivan@ya.ru>
2016-10-28 02:29:40 +0300

    сделана верстка рубрики видео

---
+++ category-video.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Шаблон Видео рубрики (category-video.php)
+ * @package WordPress
+ * @subpackage your-clean-template
+ */
+get_header(); // подключаем header.php ?>
+
+<!-- Начало рубрики-->
+
+<div class="video-category">
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+
+	<!-- начало поста -->
+	<div class="video-div">
+		<!-- контейнер картинки -->
+		<div class="video-div-img">
+			<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/video.png">
+		</div>
+		<!-- Заголовок поста -->
+			<p class="video-title">Sigelei 213 VS Fuchai 213 сравнение изнутри - Vaporplace</p>
+		<!-- ссылка смотреть -->
+		<a href="" class="video-read green">Смотреть</a>
+	</div>
+	<!-- конец поста -->
+
+</div>
+
+
+<?php pagination(); // пагинация, функция нах-ся в function.php ?>
+
+
+<?php get_sidebar(); // подключаем footer.php ?>
+<?php get_footer(); // подключаем footer.php ?>
