eb3efcf508f849640ca3e4ef1b43b0023154470e
Ivan26ru <it-1ivan@ya.ru>
2016-10-28 03:52:02 +0300

    начал верстать рубрику рецепты

---
+++ category-recipes.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Шаблон Рецепты рубрики (category-recipes.php)
+ * @package WordPress
+ * @subpackage your-clean-template
+ */
+get_header(); // подключаем header.php ?>
+
+<!-- Начало рубрики-->
+
+
+
+
+<?php pagination(); // пагинация, функция нах-ся в function.php ?>
+
+
+<?php get_sidebar(); // подключаем footer.php ?>
+<?php get_footer(); // подключаем footer.php ?>
