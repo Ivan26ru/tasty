58c44fb3c00fe2b99617015b1aec4982b350bd2f
Ivan26ru <it-1ivan@ya.ru>
2016-10-28 18:07:37 +0300

    строка поиска и кнопка добавить рецепт

--- category-recipes.php
+++ category-recipes.php
@@ -10,6 +10,20 @@ get_header(); // подключаем header.php ?>
 
 <h1 class="recipes-h1">Рецепты</h1>
 
+<!-- строка поиска и добавить рецепт -->
+<div class="search clearfix">
+		<div class="fl search-div">
+			<!-- сама форма поиска -->
+			<form class="" id="search_string" method="get" role="search" action="<?php echo home_url( '/' ) ?>" >
+				<!-- строка поиска -->
+				<input class="search-text" type="text" value="поиск" name="s" id="s">
+				<!-- кнопка поиска -->
+				<button class="search-button" type="submit" id="searchsubmit" ><i class="fa fa-search" aria-hidden="true"></i></button>
+			</form>
+		</div>
+	<a href="#" class="fr btn-green">добавить рецепт</a>
+</div>
+
 
 <?php pagination(); // пагинация, функция нах-ся в function.php ?>
 
