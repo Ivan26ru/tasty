800bedaeebca20f93c60b30afa0f46a7ee21e015
Ivan26ru <it-1ivan@ya.ru>
2016-10-29 21:10:42 +0300

    исправлены отступы в категории рецепты

--- category-recipes.php
+++ category-recipes.php
@@ -43,25 +43,60 @@ get_header(); // подключаем header.php ?>
 		</tr>
 		<!-- пост -->
 		<tr class="tr">
-			<td class="rec-td-name"><a href="#" class="podzagolovok">Hollywood</a></td>
+			<td class="rec-td-name"><a href="#" class="">Adapted by Alisa :Bust-a-CoffeeNut with Espresso</a></td>
 			<td class="rec-td-avtor">Flex</td>
-			<td class="rec-td-data">21 Июня, 21:33</td>
-			<td class="rec-td-rejting">+12</td>
+			<td class="rec-td-data">21 Июня 21:33</td>
+			<td class="rec-td-rejting">
+				<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+				<span class="green font-rr">+12</span>
+				<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+			</td>
 		</tr>
-				<!-- пост -->
+		<!-- пост -->
 		<tr class="tr">
-			<td class="rec-td-name">Hollywood</td>
+			<td class="rec-td-name"><a href="#" class="">Adapted by Alisa :Bust-a-CoffeeNut with Espresso</a></td>
 			<td class="rec-td-avtor">Flex</td>
-			<td class="rec-td-data">21 Июня, 21:33</td>
-			<td class="rec-td-rejting">+12</td>
+			<td class="rec-td-data">21 Июня 21:33</td>
+			<td class="rec-td-rejting">
+				<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+				<span class="green font-rr">+12</span>
+				<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+			</td>
 		</tr>
-				<!-- пост -->
+		<!-- пост -->
 		<tr class="tr">
-			<td class="rec-td-name">Hollywood</td>
+			<td class="rec-td-name"><a href="#" class="">Adapted by Alisa :Bust-a-CoffeeNut with Espresso</a></td>
 			<td class="rec-td-avtor">Flex</td>
-			<td class="rec-td-data">21 Июня, 21:33</td>
-			<td class="rec-td-rejting">+12</td>
+			<td class="rec-td-data">21 Июня 21:33</td>
+			<td class="rec-td-rejting">
+				<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+				<span class="green font-rr">+12</span>
+				<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+			</td>
 		</tr>
+		<!-- пост -->
+		<tr class="tr">
+			<td class="rec-td-name"><a href="#" class="">Adapted by Alisa :Bust-a-CoffeeNut with Espresso</a></td>
+			<td class="rec-td-avtor">Flex</td>
+			<td class="rec-td-data">21 Июня 21:33</td>
+			<td class="rec-td-rejting">
+				<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+				<span class="green font-rr">+12</span>
+				<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+			</td>
+		</tr>
+		<!-- пост -->
+		<tr class="tr">
+			<td class="rec-td-name"><a href="#" class="">Adapted by Alisa :Bust-a-CoffeeNut with Espresso</a></td>
+			<td class="rec-td-avtor">Flex</td>
+			<td class="rec-td-data">21 Июня 21:33</td>
+			<td class="rec-td-rejting">
+				<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
+				<span class="green font-rr">+12</span>
+				<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
+			</td>
+		</tr>
+
 	</table>
 </div>
 
