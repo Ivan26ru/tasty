1a863e89e262d23ea231859ce6ae82c6ffa73736
Ivan26ru <it-1ivan@ya.ru>
2016-10-30 03:23:44 +0300

    добавление рецепта половину сделал

--- single-recipes-admin.php
+++ single-recipes-admin.php
@@ -9,77 +9,29 @@ get_header(); // подключаем header.php ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); // старт цикла ?>
 
 <!-- начало поста -->
-<div class="post-recipes-container clearfix">
-<h1 class="recipes-h1">Рецепты админка1</h1>
-	<!-- контакты-->
-	<div class="p-recipes-div-kont">
-		<p>Автор:<span>Kimsmm</span></p>
-		<p>Дата:<span>21 июля 12:40</span></p>
-		<p>Рейтинг:
-			<span>
-				<i class="fa fa-thumbs-o-down" aria-hidden="true"></i>
-				<span class="green font-rr">+12</span>
-				<i class="fa fa-thumbs-o-up" aria-hidden="true"></i>
-			</span>
+<div class="recipes-admin-div">
+	<form action="#" class="text-center form-elements">
+		<input type="text" name="name-recipes" id="" class="input-name" placeholder="Введите имя">
+		<div class="w263">
+
+		<p class="elements-input"><label><span class="name-element">Amount to make</span><input class="input-ml" type="search" name="site_search"><span class="ml">ml</span></label></p>
+		<p class="elements-input"><label><span class="name-element">Amount to make</span><input class="input-ml" type="search" name="site_search"><span class="ml">ml</span></label></p>
+		<p class="elements-input"><label><span class="name-element">Amount to make</span><input class="input-ml" type="search" name="site_search"><span class="ml">ml</span></label></p>
+
+		<p class="elements-input-new">
+			<input class="input-name-el" type="search" name="element-new" placeholder="Ингридиент 1">
+			<input class="input-ml" type="search" name="site_search"><span class="ml">ml</span>
 		</p>
-	</div>
-	<!-- таблица ингридиенты -->
-	<table class="post-recipes-ingredient">
-		<tr>
-			<th><span>INGREDIENT</span></th>
-			<th><span>ml</span></th>
-			<th><span>GRAMS</span></th>
-			<th><span>%</span></th>
-		</tr>
-		<!-- содержимое таблицы -->
-		<!-- строка -->
-		<tr>
-			<td>Nicotine juice 72 mg (100% PG)</td>
-			<td>1.67</td>
-			<td>1.73</td>
-			<td>5.57</td>
-		</tr>
-				<!-- строка -->
-		<tr>
-			<td>Nicotine juice 72 mg (100% PG)</td>
-			<td>1.67</td>
-			<td>1.73</td>
-			<td>5.57</td>
-		</tr>
-				<!-- строка -->
-		<tr>
-			<td>Nicotine juice 72 mg (100% PG)</td>
-			<td>1.67</td>
-			<td>1.73</td>
-			<td>5.57</td>
-		</tr>
-				<!-- строка -->
-		<tr>
-			<td>Nicotine juice 72 mg (100% PG)</td>
-			<td>1.67</td>
-			<td>1.73</td>
-			<td>5.57</td>
-		</tr>
-	</table>
 
-	<div class="o-post-recipes mt33">
-		<!-- описание поста -->
-		<p class="podzagolovok"><span class="line">Описание</span></p>
-		<div class="text-other">
-			<?php add_filter('the_content','htm_image_content_filter',11); ?>
-<?php the_content(); // контент без картинок?>
+		<a href="#" class="btn-green">добавить ингридиент</a>
+<br>
+		<textarea class="opisanie" name="opisanie" id="" cols="30" rows="10" placeholder="Добавить описание"></textarea>
 		</div>
-
-	</div>
+	</form>
 </div>
 
 <?php endwhile; // конец цикла ?>
 
-
-<div class="o-post-recipes-comment">
-<!-- конец поста -->
-<?php if (comments_open() || get_comments_number()) comments_template('', true); // если комментирование открыто - мы покажем список комментариев и форму, если закрыто, но кол-во комментов > 0 - покажем только список комментариев ?>
-</div>
 
 <?php get_sidebar(); // подключаем footer.php ?>
 <?php get_footer(); // подключаем footer.php ?>
