c9151c11d43f303a668aace538200f530cd5c659
Ivan26ru <it-1ivan@ya.ru>
2016-11-16 21:26:53 +0300

    разрешен ввод только цифр

--- js/main.js
+++ js/main.js
@@ -47,7 +47,26 @@ $(document).ready(function() {
         })
         // конец табов
 
+// разрешен ввод только цифр
 
+    $(".input-ml").keydown(function(event) {
+        // Разрешаем нажатие клавиш backspace, Del, Tab и Esc
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+             // Разрешаем выделение: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) ||
+             // Разрешаем клавиши навигации: Home, End, Left, Right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 return;
+        }
+        else {
+            // Запрещаем всё, кроме клавиш цифр на основной клавиатуре, а также Num-клавиатуре
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault();
+            }
+        }
+    });
+
+// .разрешен ввод только цифр
 
 
 
