d49c75d8ebac6480068bb618afa1649376fb41f7
Ivan26ru <it-1ivan@ya.ru>
2016-11-30 16:46:29 +0300

    подключил localstorage на js запоминает выбор

--- js/main.js
+++ js/main.js
@@ -14,17 +14,29 @@ jQuery(document).ready(function( $ ) {
 
 // ФОРМА ПРОВЕРКИ СОВЕРШЕННОЛЕТИЯ
 
+// условие отвечал раньше или нет
+if (!localStorage.getItem('year-18')) {
+
+    // появления окна вопроса о возрасте
+$('#window-18, #tabs-dark').fadeIn(300);
+
+
 // нажатие на кнопку да
 $('#w-18-yes').click(function(event){//действие при клике
     event.preventDefault();//отмена обычного поведения
     $('#window-18, #tabs-dark').slideUp(500);//эфект скрытия
+    localStorage.setItem('year-18', true);
 });
+
 
 // нажатие на кнопку нет
 $('#w-18-no').click(function(event){//действие при клике
     event.preventDefault();//отмена обычного поведения
     window.location.href = "http://mail.ru/";
 });
+
+
+};
 
 
 
--- vhod_wp.php
+++ vhod_wp.php
@@ -1,9 +1,9 @@
 <!-- начало табов -->
-<div class="window-18" id="window-18">
+<div class="window-18 dn" id="window-18">
 	<p class="window-18-title zagolovok line">Предупреждение</p>
 	<p class="window-18-info">Страницы, которые Вы собираетесь открыть, можут содержать материалы, предназначенные только для взрослых. Чтобы продолжить, Вы должны подтвердить, что Вам уже исполнилось 18 лет.</p>
 	<a class="window-18-yes btn-green" id="w-18-yes" href="#">Да, мне уже есть 18 лет!</a>
 	<a class="window-18-no btn-green" id="w-18-no" href="#">Нет, я младше 18 лет</a>
 </div>
 <!-- затемняющий фон -->
-<div class="tabs-dark" id="tabs-dark"></div>
+<div class="tabs-dark dn" id="tabs-dark"></div>
