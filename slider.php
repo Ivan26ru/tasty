4a1dc126ebf79bed78aa411d87a629c9237baedd
Ivan26ru <it-1ivan@ya.ru>
2016-12-04 14:44:55 +0300

    поставлены 3 одинаковые картинки

--- slider.php
+++ slider.php
@@ -9,30 +9,11 @@
 
 <!-- блок для слайдера -->
 <div class="slider-block">
-<div id="owl-example" class="owl-carousel">
-  <div><img src="http://placehold.it/1170x300/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x400/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x500/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x200/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x500/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x250/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x350/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x300/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x100/42bdc2/FFFFFF"></div>
-  <div><img src="http://placehold.it/1170x500/42bdc2/FFFFFF"></div>
-  </div>
+	<div id="owl-example" class="owl-carousel">
+		<div><img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки"></div>
+		<div><img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки"></div>
+		<div><img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки"></div>
+	</div>
 </div>
-<!--
-			<img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки">
-	</div>
-	<div id="owl-example" class="owl-carousel">
-			<img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки">
-	</div>
-	<div id="owl-example" class="owl-carousel">
-			<img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки">
-	</div>
-	<div id="owl-example" class="owl-carousel">
-			<img src="<?php echo get_template_directory_uri(); //путь до темы; ?>/img/png/slider/slide1.png" alt="описание картинки">
-	 -->
 
 <!-- .слайдер -->
