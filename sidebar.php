6a985d96be8783cb039e9bf8b9aebf378c40fda3
ivan26ru <it-1ivan@yadnex.ru>
2017-03-28 04:17:31 +0300

    изменил ссылки на tasty.v-svetlograde.ru

--- sidebar.php
+++ sidebar.php
@@ -20,7 +20,7 @@ $slider_post = 955;
 <!-- top5 -->
 <div class="sb-r top5">
 	<img src="<?php echo get_template_directory_uri(); // абсолютный путь до темы ?>/img/png/top5.png" class="sidebar-img">
-<!-- 	<a href="http://tastyvape.ru/vipe-fest/">Обзор жидкости ZE-PAR – Креатив и фантазия и все такое</a>
+<!-- 	<a href="http://tasty.v-svetlograde.ru/vipe-fest/">Обзор жидкости ZE-PAR – Креатив и фантазия и все такое</a>
 	<a href="#">Обзор жидкости ZE-PAR – Креатив и фантазия и все такое</a>
 	<a href="#">Обзор жидкости ZE-PAR – Креатив и фантазия и все такое</a>
 	<a href="#">Обзор жидкости ZE-PAR – Креатив и фантазия и все такое</a>
@@ -32,12 +32,12 @@ $slider_post = 955;
 <!-- лучшие девайсы -->
 <div class="best-device">
 	<p class="zagolovok"><a href="<?php echo get_category_link( '10' ); ?>"><span class="line">лучшие девайсы</span></a></p>
-	<a href="http://tastyvape.ru/category/best_device/box_mods/" class="best-device-a d1"><span>BOX MODS</span></a>
-	<a href="http://tastyvape.ru/category/best_device/mechanical_mod/" class="best-device-a d2"><span>Mechanical  Mod</span></a>
-	<a href="http://tastyvape.ru/category/best_device/rdas/" class="best-device-a d3"><span>RDas</span></a>
-	<a href="http://tastyvape.ru/category/best_device/sub_ohm_tanks/" class="best-device-a d4"><span>Sub ohm  tanks</span></a>
-	<a href="http://tastyvape.ru/category/best_device/battary_chargers/" class="best-device-a d5"><span>battery  chargers</span></a>
-	<a href="http://tastyvape.ru/category/best_device/batteries/" class="best-device-a d6"><span>batteries</span></a>
+	<a href="http://tasty.v-svetlograde.ru/category/best_device/box_mods/" class="best-device-a d1"><span>BOX MODS</span></a>
+	<a href="http://tasty.v-svetlograde.ru/category/best_device/mechanical_mod/" class="best-device-a d2"><span>Mechanical  Mod</span></a>
+	<a href="http://tasty.v-svetlograde.ru/category/best_device/rdas/" class="best-device-a d3"><span>RDas</span></a>
+	<a href="http://tasty.v-svetlograde.ru/category/best_device/sub_ohm_tanks/" class="best-device-a d4"><span>Sub ohm  tanks</span></a>
+	<a href="http://tasty.v-svetlograde.ru/category/best_device/battary_chargers/" class="best-device-a d5"><span>battery  chargers</span></a>
+	<a href="http://tasty.v-svetlograde.ru/category/best_device/batteries/" class="best-device-a d6"><span>batteries</span></a>
 </div>
 
 <!-- рекламный блок -->
